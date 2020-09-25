9a2c36030943e9fab3edae379601e4f3c142b78f
JeffreyTam8 <60583595+JeffreyTam8@users.noreply.github.com>
2020-09-25 18:52:41 -0400

    Add files via upload

---
+++ Tam_Jeffrey_Lab5_GUI/DataManager.java
@@ -0,0 +1,43 @@
+/*
+ * Jeffrey Tam
+ * 9-25-2020
+ * CMSC 203 21559  
+ * Lab 5
+ * 
+ */
+
+package application;
+/**
+ * The DataManager class should never depend on the GUI, but rather the reverse. 
+ * So the DataManager methods should not use the GUI directly.  If you want data
+ * to get from the user to the manager, have the GUI get the data, and call the manager 
+ * with the data that the GUI got from a text field or other data structure.
+ * @author ralexander
+ *
+ */
+public class DataManager {
+    
+	DataManager() {	}
+
+	/** 
+	 * This method illustrates how the GUI can interact with the manager
+	 */
+	public String getHello() {
+		return "Hello World";
+	}
+
+	public String getHowdy() {
+		return "Howdy y'all";
+	}
+
+	public String getChinese() {
+		return "Ni hau";
+	}
+
+	//Task #4 Optional
+	
+	public String getFrench() {
+		return "Bonjour";
+	}
+
+}
